77d86e50acf48bc07ebc3fb2499f3299bfe96611
Taniya Hinduja <taniyahinduja11@gmail.com>
2022-01-24 21:53:37 +0530

    Correction

--- Day1/AssignmentCodes/Question1.cpp
+++ Day1/AssignmentCodes/Question1.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int* reverseArray(vector<int> P)
+int *reverseArray(vector<int> P)
 {
-    int res[P.size()];
+    int *res = new int[P.size()];
     int c = 0;
     for (int i = P.size() - 1; i >= 0; i--)
     {
         res[c] = (int)P[i];
         c++;
     }
-    for (int i = 0; i < P.size(); i++)
-        cout << res[i] << " ";
     return res;
 }
 int main()
@@ -22,8 +20,8 @@ int main()
     vector<int> P(Q);
     for (int i = 0; i < Q; i++)
         cin >> P[i];
-    
-    int* res = reverseArray(P);
+
+    int *res = reverseArray(P);
     for (int i = 0; i < Q; i++)
         cout << res[i] << " ";
     return 0;
